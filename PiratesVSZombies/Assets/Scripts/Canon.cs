df1e194ea60e56e58cdd807cab29090eb26d6668
Fran <fralg100@gmail.com>
2019-12-28 14:12:16 +0100

    Disparos

---
+++ PiratesVSZombies/Assets/Scripts/Canon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : MonoBehaviour {
+
+    public GameObject bala;
+    public Transform origen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            GameObject proyectil = Instantiate(bala);
+            proyectil.transform.position = origen.position;
+            Rigidbody rb = proyectil.GetComponent<Rigidbody>();
+            //rb.velocity=Camera.main.transform.forward * 30;
+            rb.AddForce(Camera.main.transform.forward * 20000);
+        }
+	}
+}
