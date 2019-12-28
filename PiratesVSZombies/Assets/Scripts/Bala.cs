a83f51206d8622583c72e360e1be87cfcebab104
Fran <fralg100@gmail.com>
2019-12-28 19:11:56 +0100

    Patrulla zombie

---
+++ PiratesVSZombies/Assets/Scripts/Bala.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour {
+
+    bool activo = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ambiente")
+            activo = false;
+        if (col.gameObject.tag == "Enemigo" && this.gameObject.GetComponent<Rigidbody>().velocity.magnitude>8 && activo)
+        {
+            Debug.Log("Enemigo tocado "+ this.gameObject.GetComponent<Rigidbody>().velocity.magnitude);
+            col.gameObject.GetComponent<PatrullaZombie>().enabled = false;
+            col.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+        }
+    }
+}
---
+++ PiratesVSZombies/Assets/Scripts/PatrullaZombie.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatrullaZombie : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform[] ruta;
+    int puntoDeRuta;
+    UnityEngine.AI.NavMeshAgent agent;
+    public GameObject player;
+    public bool perseguir;
+
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.autoBraking = false;
+        SiguientePunto();
+    }
+
+    void SiguientePunto()
+    {
+        if (ruta.Length == 0)
+        {
+            return;
+        }
+        agent.destination = ruta[puntoDeRuta].position;
+        puntoDeRuta = (puntoDeRuta + 1) % ruta.Length;
+    }
+
+    void perseguirJugador() {
+
+        agent.destination = player.transform.position;
+
+    }
+
+
+    void Update()
+    {
+        if (perseguir)
+            perseguirJugador();
+        else
+        {
+            if (agent.remainingDistance < 0.5f)
+            {
+                SiguientePunto();
+            }
+        }
+    }
+}
